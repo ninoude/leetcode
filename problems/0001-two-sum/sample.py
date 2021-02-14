74c793ce6c4c5cac94d3d37ecaebd169789c377c
ninoude <ninoudeaoi@gmail.com>
2021-02-14 12:43:55 +0000

    update

--- problems/0001-two-sum/sample.py
+++ problems/0001-two-sum/sample.py
@@ -7,19 +7,6 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         output = []
-        for i in range(len(nums)):
-            output = []
-            if ((target - nums[i]) in nums and nums.index(target - nums[i]) > i):
-                output.append(i)
-                output.append(nums.index(target - nums[i]))
-                print(output)
-        return output
-
-#nums1 = [2,7,11,15]
-#target1 = 9
-#nums2 = [3,2,4]
-#target2 = 6
-nums = [2,7,11,15]
-target = 9
-instance = Solution()
-print(instance.twoSum(nums, target))
+        for i, x in enumerate (nums):
+            if target - x in nums[i+1:]:
+                return [i, nums[i+1:].index(target-x)+i+1]
