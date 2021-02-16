4b18f8f55a04f9461682beae7e9cf597f22b6076
ninoude <ninoudeaoi@gmail.com>
2021-02-16 14:57:44 +0000

    update

--- problems/0002-add-two-numbers/sample.py
+++ problems/0002-add-two-numbers/sample.py
@@ -13,16 +13,48 @@ class Solution:
         instance = ListNode()
         current = instance
         carry = 0
-        length = 3
+        l = len(l1) - len(l2)
+
+        if l > 0:
+            arr = [0] * (l)
+            l1.extend([0])
+            l2.extend(arr)
+        elif l < 0:
+            arr = [0] * (-l)
+            l2.extend([0])
+            l1.extend(arr)
+        else:
+            l1.extend([0])
+            l2.extend([0])
+
+        length = int((len(l1) + len(l2))/2)
 
         for i in range(length):
             total = l1[i] + l2[i] + carry
             carry = int(total/10)
             current.next = ListNode(total%10)
             current = current.next
-        return instance
 
-l1 = [2,4,3]
-l2 = [5,6,4]
+        if carry==1:
+            current.next = ListNode(1)
+            current = current.next
+
+        return instance.next
+
+# output = 708
+#l1 = [2,4,3]
+#l2 = [5,6,4]
+# output = 0
+#l1 = [0]
+#l2 = [0]
+# output = 89990001
+l2 = [9,9,9,9,9,9,9]
+l1 = [9,9,9,9]
 instance = Solution()
 obj = instance.addTwoNumbers(l1, l2)
+
+while obj.next != None:
+    print(obj.val, end='')
+    obj = obj.next
+print(obj.val, end='')
+
