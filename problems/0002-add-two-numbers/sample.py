b91b29c1fd84cd6b8d92dc2fe60d0d98546fe816
ninoude <ninoudeaoi@gmail.com>
2021-02-18 15:03:15 +0000

    update

--- problems/0002-add-two-numbers/sample.py
+++ problems/0002-add-two-numbers/sample.py
@@ -1,12 +1,10 @@
 #Input: l1 = [2,4,3], l2 = [5,6,4]
 #Output: [7,0,8]
 #Explanation: 342 + 465 = 807.
-from IPython import embed
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+#class ListNode:
+#    def __init__(self, val=0, next=None):
+#        self.val = val
+#        self.next = next
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
@@ -14,14 +12,14 @@ class Solution:
         current = instance
         carry = 0
 
-        while l1.next != None and l2.next != None:
-            if l1.next != None:
+        while l1 is not None or l2 is not None:
+            if l1 is not None:
                 l1_v = l1.val
                 l1 = l1.next
             else:
                 l1_v=0
 
-            if l2.next != None:
+            if l2 is not None:
                 l2_v = l2.val
                 l2 = l2.next
             else:
@@ -37,35 +35,3 @@ class Solution:
             current = current.next
 
         return instance.next
-
-# output = 708
-#l1_list = [2,4,3]
-#l2_list = [5,6,4]
-# output = 0
-l1_list = [0]
-l2_list = [0]
-# output = 89990001
-#l2 = [9,9,9,9,9,9,9]
-#l1 = [9,9,9,9]
-
-l1 = ListNode()
-l1_current = l1
-l2 = ListNode()
-l2_current = l2
-
-for i in range(len(l1_list)):
-    l1_current.next = ListNode(l1_list[i])
-    l1_current = l1_current.next
-
-for i in range(len(l2_list)):
-    l2_current.next = ListNode(l2_list[i])
-    l2_current = l2_current.next
-
-instance = Solution()
-obj = instance.addTwoNumbers(l1.next, l2.next)
-
-while obj.next != None:
-    print(obj.val, end='')
-    obj = obj.next
-print(obj.val, end='')
-
