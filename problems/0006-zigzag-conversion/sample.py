c7b3c7cc52097a38b6c4c37960f8b71a3b666c5e
ninoude <ninoudeaoi@gmail.com>
2021-06-16 15:28:43 +0000

    Fix codes

--- problems/0006-zigzag-conversion/sample.py
+++ problems/0006-zigzag-conversion/sample.py
@@ -1,26 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if len(s) <= numRows: return s
+        if len(s) < numRows or numRows == 1: return s
 
-        k = i = 0
-        j = numRows - 2
-        list = [[] for i in range(numRows)]
-        answer = ''
-        flag = 0
+        l = 0
+        p = 1
+        arr = [""] * numRows
+        output = ""
+        
+        for i in range (len(s)):
+            arr[l] += s[i]
+            l += p
+            if l == 0 or l == numRows -1:
+                p *= -1
+        
+        for j in range (numRows):
+            output += arr[j]
 
-        while i < len(s):
-            list[k%(numRows)].append(s[i])
-            i+=1
-            k+=1
-            if k%numRows == 0 and 0 < i: flag = 1
-            while 0 < j and flag == 1 and i < len(s):
-                list[j].append(s[i])
-                j-=1
-                i+=1
-                if j == 0:
-                    k=0
-                    flag = 0
-                    j = numRows - 2
-        for i in range(numRows):
-            answer = answer + ''.join(list[i])
-        return answer
+        return output
