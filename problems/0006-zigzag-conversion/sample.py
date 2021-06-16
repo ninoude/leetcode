2fca360d90da0ec48d86809ca43a5fb8d3ac98a2
ninoude <ninoudeaoi@gmail.com>
2021-06-16 14:31:53 +0000

    Add 6

---
+++ problems/0006-zigzag-conversion/sample.py
@@ -0,0 +1,27 @@
+from IPython import embed
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if len(s) <= numRows: return s
+
+        k = i = 0
+        j = numRows - 2
+        list = [[] for i in range(numRows)]
+        answer = ''
+        flag = 0
+
+        while i < len(s):
+            list[k%(numRows)].append(s[i])
+            i+=1
+            k+=1
+            if k%numRows == 0 and 0 < i: flag = 1
+            while 0 < j and flag == 1 and i < len(s):
+                list[j].append(s[i])
+                j-=1
+                i+=1
+                if j == 0:
+                    k=0
+                    flag = 0
+                    j = numRows - 2
+        for i in range(numRows):
+            answer = answer + ''.join(list[i])
+        return answer
