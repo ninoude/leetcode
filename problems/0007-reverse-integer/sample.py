e688e72dd49aba05645343a1756d92c064a07a32
ninoude <ninoudeaoi@gmail.com>
2021-06-17 01:07:36 +0000

    Add 7

---
+++ problems/0007-reverse-integer/sample.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        x_str = ""
+        flag_minus = 0
+        flag_zero = 0
+        output = 0
+        if x < 0:
+            x *=-1
+            flag_minus = 1
+            
+        x_str = str(x)
+        output_str = x_str[::-1]
+
+        if flag_minus == 1:
+            output = int(output_str)
+            output *= -1
+        else:
+            output = int(output_str)
+
+        if output < -2147483648 or 2147483647 < output:
+            return 0
+        return output
