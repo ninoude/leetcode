be7692a5a679e4c4aaea430ee0e350a6b10f7584
ninoude <ninoudeaoi@gmail.com>
2021-06-17 01:12:18 +0000

    Fix codes

--- problems/0007-reverse-integer/sample.py
+++ problems/0007-reverse-integer/sample.py
@@ -17,6 +17,7 @@ class Solution:
         else:
             output = int(output_str)
 
-        if output < -2147483648 or 2147483647 < output:
+#        if output < -2**31 or 2**31-1 < output:
+         if output < -2147483648 or 2147483647 < output:
             return 0
         return output
