237e60f925c4e1e13eca47f52b3bda2b52006c1f
ninoude <ninoude@users.noreply.github.com>
2021-09-23 23:04:13 +0900

    Improve 17

--- problems/0017-letter-combinations-of-a-phone-number/sample.py
+++ problems/0017-letter-combinations-of-a-phone-number/sample.py
@@ -3,24 +3,19 @@ from typing import List
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if len(digits) == 0: return []
-        mapping = {"1": "",     "2": "abc", "3": "def",
+        buttons = {"1": "",     "2": "abc", "3": "def",
                    "4": "ghi",  "5": "jkl", "6": "mno",
                    "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        answer = []
+        answer = [""]
 
         for n in digits:
-            answer = self.product(answer, mapping[n])
+            answer = self.product(answer, buttons[n])
         return answer
 
     def product(self, nums1: List[str], nums2: str):
-        answer = []
-        if len(nums1) == 0:
-            for n in nums2:
-                answer.append(n)
-            return answer
-
+        arr = []
         for n1 in nums1:
             for n2 in nums2:
-                answer.append(n1+n2)
-        return answer
+                arr.append(n1+n2)
+        return arr
 
