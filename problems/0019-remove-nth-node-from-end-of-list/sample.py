51d7dcf5f83bf8cb57b78080597f2d58af153354
ninoude <ninoude@users.noreply.github.com>
2021-10-16 20:32:41 +0900

    Remove debug code

--- problems/0019-remove-nth-node-from-end-of-list/sample.py
+++ problems/0019-remove-nth-node-from-end-of-list/sample.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+#class ListNode:
+#    def __init__(self, val=0, next=None):
+#        self.val = val
+#        self.next = next
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
@@ -38,47 +38,3 @@ class Solution:
             answer_current.next = ListNode(current.val)
             return answer.next
 
-l1_list = [1,2,3,4,5]
-l1 = ListNode()
-l1_current = l1
-
-for i in range(len(l1_list)):
-    l1_current.next = ListNode(l1_list[i])
-    l1_current = l1_current.next
-
-test = Solution()
-answer = test.removeNthFromEnd(l1.next, 2)
-
-for i in range (len(l1_list)-1):
-    print(answer.val)
-    answer = answer.next
-
-l1_list = [1]
-l1 = ListNode()
-l1_current = l1
-
-for i in range(len(l1_list)):
-    l1_current.next = ListNode(l1_list[i])
-    l1_current = l1_current.next
-
-test = Solution()
-answer = test.removeNthFromEnd(l1.next, 1)
-
-for i in range (len(l1_list)-1):
-    print(answer.val)
-    answer = answer.next
-
-l1_list = [1,2]
-l1 = ListNode()
-l1_current = l1
-
-for i in range(len(l1_list)):
-    l1_current.next = ListNode(l1_list[i])
-    l1_current = l1_current.next
-
-test = Solution()
-answer = test.removeNthFromEnd(l1.next, 1)
-
-for i in range (len(l1_list)-1):
-    print(answer.val)
-    answer = answer.next
