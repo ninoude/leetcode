ad7423f875ab2ecfa4fd6bed4f38ab37d634cb72
ninoude <ninoude@users.noreply.github.com>
2022-01-22 15:22:59 +0900

    Remove debug code

--- problems/0024-swap-nodes-in-pairs/sample.py
+++ problems/0024-swap-nodes-in-pairs/sample.py
@@ -2,10 +2,10 @@ from typing import List
 from typing import Optional
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+#class ListNode:
+#    def __init__(self, val=0, next=None):
+#        self.val = val
+#        self.next = next
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -24,34 +24,3 @@ class Solution:
             point = point.next
         return answer.next
 
-l1_list = [1,2,3,4]
-l1 = ListNode()
-l1_current = l1
-
-for i in range(len(l1_list)):
-    l1_current.next = ListNode(l1_list[i])
-    l1_current = l1_current.next
-
-test = Solution()
-answer = test.swapPairs(l1.next)
-
-print('[2,1,4,3]')
-while answer:
-    print(answer.val)
-    answer = answer.next
-
-l1_list = [1]
-l1 = ListNode()
-l1_current = l1
-
-for i in range(len(l1_list)):
-    l1_current.next = ListNode(l1_list[i])
-    l1_current = l1_current.next
-
-test = Solution()
-answer = test.swapPairs(l1.next)
-
-print('[1]')
-while answer:
-    print(answer.val)
-    answer = answer.next
